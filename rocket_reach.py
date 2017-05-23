8ed89874d8e67f0b7fdb03dba2ae4b2b551d5f53
Shehroze Bhatti <shehrozebhatti@Shehrozes-MacBook-Pro-9.local>
2017-05-23 17:05:21 +0100

    new changes

--- rocket_reach.py
+++ rocket_reach.py
@@ -2,14 +2,27 @@
 # Save your Linkedin profile links in 'linkedinlinks.txt' (one link per line). 
 # Also you need to fill in your API key in the script.
 
-import requests, json
-url = "https://api.rocketreach.co/v1/api/lookupProfile?api_key=[YOUR_API_KEY]&li_url="
+import requests, json, time
+url = "https://api.rocketreach.co/v1/api/lookupProfile?api_key=77377k10d4cccb39188a803dc4ab19f4fc1c5a&li_url="
 lines = [line.rstrip('\n') for line in open('linkedinlinks.txt')]
+fileToWrite = open('emails.txt', 'w')
 for line in lines:
     fullurl = url+line
     data = requests.get(fullurl)
+    time.sleep(2)
     json_data=data.text
     data = json.loads(json_data)
-    emails = [item['email'] for item in data[0]['emails']]
+    try:
+        emails = [item['email'] for item in data[0]['emails']]
+    except:
+        emails = []
+    emailText = ''
+    for i,email in enumerate(emails):
+        if(i == 0):
+            emailText = emailText + email
+        else:
+            emailText = emailText + email + ','
+    fileToWrite.write(emailText)
+    fileToWrite.write('\n')
     print emails
-    time.sleep(1)
+fileToWrite.close()
